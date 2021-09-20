12ec3c2d9812328937ed616e0bbfbd87e7af92e8
cunknown <48595746+anonymous14725@users.noreply.github.com>
2021-09-21 02:33:51 +0430

    Update index.js

--- index.js
+++ index.js
@@ -2,31 +2,35 @@ var i = 0;
 var txt = 'for hackers of the professional and beginner';
 var speed = 50;
 
-function typing(){
-    if (i < txt.length){
+function typing()
+{
+    if (i < txt.length)
+    {
         document.getElementById("head").innerHTML += txt.charAt(i);
         i++;
         setTimeout(typing, speed);
     }
 }
 
-function mouse(event){
+function mouse(event)
+{
     var x = event.clientX;
     var y = event.clientY;
-    document.getElementById("msmn").style.transition = "0.5s ease";
-    document.getElementById("msmn").style.background = "white";
-    document.getElementById("msmn").style.filter = "blur(15px)";
-    document.getElementById("msmn").style.position = "sticky";
-    document.getElementById("msmn").style.top = y + "px";
-    document.getElementById("msmn").style.left = x + "px";
-    document.getElementById("msmn").style.width = "50px";
-    document.getElementById("msmn").style.height = "50px";
-    document.getElementById("msmn").style.borderRadius = "900px";
-    document.getElementById("msmn").style.boxShadow = "0px 0px 40px white";
+    document.getElementById("moving").style.transition = "0.5s ease";
+    document.getElementById("moving").style.background = "white";
+    document.getElementById("moving").style.filter = "blur(15px)";
+    document.getElementById("moving").style.position = "sticky";
+    document.getElementById("moving").style.top = y + "px";
+    document.getElementById("moving").style.left = x + "px";
+    document.getElementById("moving").style.width = "50px";
+    document.getElementById("moving").style.height = "50px";
+    document.getElementById("moving").style.borderRadius = "900px";
+    document.getElementById("moving").style.boxShadow = "0px 0px 40px white";
 }
-function mouseleave(){
-    document.getElementById("msmn").style.width = "50px";
-    document.getElementById("msmn").style.height = "50px";
-    document.getElementById("msmn").style.background = "transparent";
-    document.getElementById("msmn").style.boxShadow = "none";
+function mouseleave()
+{
+    document.getElementById("moving").style.width = "50px";
+    document.getElementById("moving").style.height = "50px";
+    document.getElementById("moving").style.background = "transparent";
+    document.getElementById("moving").style.boxShadow = "none";
 }
