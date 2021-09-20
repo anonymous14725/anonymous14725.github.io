35d3597ac24ab19db874ca2b4901c8564d0ba86b
cunknown <48595746+anonymous14725@users.noreply.github.com>
2021-09-21 01:15:26 +0430

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-function character(text,speed){
+function character(id,text,speed){
   var i = 0;
   var txt = text;
   var speed = speed;
 
   function typeWriter() {
     if (i < txt.length) {
-      document.getElementById("demo").innerHTML += txt.charAt(i);
+      document.getElementById(id).innerHTML += txt.charAt(i);
       i++;
       setTimeout(typeWriter, speed);
     }
