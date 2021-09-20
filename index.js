7cf314c15082be17620c4cc2aa328c3909c81ce2
cunknown <48595746+anonymous14725@users.noreply.github.com>
2021-09-21 01:22:49 +0430

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 var i = 0;
-var txt = 'Lorem ipsum typing effect!'; /* The text */
-var speed = 50; /* The speed/duration of the effect in milliseconds */
-
 function character(id,txt,speed) {
   if (i < txt.length) {
     document.getElementById(id).innerHTML += txt.charAt(i);
