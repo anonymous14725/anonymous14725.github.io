d89a36875412484f3a07a45283d5a6d40fdc581f
cunknown <48595746+anonymous14725@users.noreply.github.com>
2021-09-21 02:32:14 +0430

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,24 @@ function typing(){
         setTimeout(typing, speed);
     }
 }
+
+function mouse(event){
+    var x = event.clientX;
+    var y = event.clientY;
+    document.getElementById("msmn").style.transition = "0.5s ease";
+    document.getElementById("msmn").style.background = "white";
+    document.getElementById("msmn").style.filter = "blur(15px)";
+    document.getElementById("msmn").style.position = "sticky";
+    document.getElementById("msmn").style.top = y + "px";
+    document.getElementById("msmn").style.left = x + "px";
+    document.getElementById("msmn").style.width = "50px";
+    document.getElementById("msmn").style.height = "50px";
+    document.getElementById("msmn").style.borderRadius = "900px";
+    document.getElementById("msmn").style.boxShadow = "0px 0px 40px white";
+}
+function demenu(){
+    document.getElementById("msmn").style.width = "50px";
+    document.getElementById("msmn").style.height = "50px";
+    document.getElementById("msmn").style.background = "transparent";
+    document.getElementById("msmn").style.boxShadow = "none";
+}
