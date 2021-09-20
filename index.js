3ae343cb75ea29147c4e46c8440d300e7b54c2d7
cunknown <48595746+anonymous14725@users.noreply.github.com>
2021-09-21 02:53:07 +0430

    Update index.js

--- index.js
+++ index.js
@@ -9,26 +9,3 @@ function typing(){
     }
 }
 
-function mouse(event)
-{
-    var x = event.clientX;
-    var y = event.clientY;
-    document.getElementById("moving").style.transition = "0.5s ease";
-    document.getElementById("moving").style.background = "white";
-    document.getElementById("moving").style.filter = "blur(15px)";
-    document.getElementById("moving").style.position = "sticky";
-    document.getElementById("moving").style.top = y + "px";
-    document.getElementById("moving").style.left = x + "px";
-    document.getElementById("moving").style.width = "50px";
-    document.getElementById("moving").style.height = "50px";
-    document.getElementById("moving").style.borderRadius = "900px";
-    document.getElementById("moving").style.boxShadow = "0px 0px 40px white";
-}
-
-function mouseleave()
-{
-    document.getElementById("moving").style.width = "50px";
-    document.getElementById("moving").style.height = "50px";
-    document.getElementById("moving").style.background = "transparent";
-    document.getElementById("moving").style.boxShadow = "none";
-}
