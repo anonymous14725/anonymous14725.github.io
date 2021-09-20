256ba35bd501d4db6cdbb1b2496423469386c07d
cunknown <48595746+anonymous14725@users.noreply.github.com>
2021-09-21 02:46:38 +0430

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ var i = 0;
 var txt = 'for hackers of the professional and beginner';
 var speed = 50;
 
-function typing()
-{
+document.addEventListener("load",function typing(){
     if (i < txt.length)
     {
         document.getElementById("head").innerHTML += txt.charAt(i);
         i++;
         setTimeout(typing, speed);
     }
-}
+});
 
 function mouse(event)
 {
