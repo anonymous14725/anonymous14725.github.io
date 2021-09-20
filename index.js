87835bf572f470180228be83b3039f02f880abe1
cunknown <48595746+anonymous14725@users.noreply.github.com>
2021-09-21 01:22:31 +0430

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-function character(id,text,speed){
-  var i = 0;
-  var txt = text;
-  var speed = speed;
+var i = 0;
+var txt = 'Lorem ipsum typing effect!'; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
 
-  function typeWriter() {
-    if (i < txt.length) {
-      document.getElementById(id).innerHTML += txt.charAt(i);
-      i++;
-      setTimeout(typeWriter, speed);
-    }
+function character(id,txt,speed) {
+  if (i < txt.length) {
+    document.getElementById(id).innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
   }
 }
