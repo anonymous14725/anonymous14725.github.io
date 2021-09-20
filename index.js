2e7f60e0d3964ec948d3ee2624b08b0da78912ae
cunknown <48595746+anonymous14725@users.noreply.github.com>
2021-09-21 01:10:40 +0430

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+function Character(text,speed){
+  var i = 0;
+  var txt = text;
+  var speed = speed;
+
+  function typeWriter() {
+    if (i < txt.length) {
+      document.getElementById("demo").innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    }
+  }
+}
