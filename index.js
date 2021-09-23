28a714a38825f2b9e031d7008ec36d0a0c4aa5a9
cunknown <48595746+anonymous14725@users.noreply.github.com>
2021-09-23 23:19:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 var i = 0;
 var txt = 'for hackers of the professional and beginner';
 var speed = 70;
-function typing(){
-    if (i < txt.length){
+function typing()
+{
+    if (i < txt.length)
+    {
         document.getElementById("head").innerHTML += txt.charAt(i);
         i++;
         setTimeout(typing, speed);
     }
 }
 
-function mouse(event){
+function mouse(event)
+{
     var x = event.clientX;
     var y = event.clientY;
     document.getElementById("msmn").style.transition = "0.5s ease";
@@ -25,7 +28,8 @@ function mouse(event){
     document.getElementById("msmn").style.boxShadow = "0px 0px 20px white";
 }
 
-function mouseleave(){
+function mouseleave()
+{
     document.getElementById("msmn").style.width = "20px";
     document.getElementById("msmn").style.height = "20px";
     document.getElementById("msmn").style.background = "transparent";
